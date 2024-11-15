cf73697381da7bbe058d404526fda1261f580f5f
Nahuel-NP <pedroso.nahuel.dev@gmail.com>
2024-11-15 11:44:48 -0300

    change to hybrid generation

--- src/pages/api/posts/index.ts
+++ src/pages/api/posts/index.ts
@@ -1,10 +1,13 @@
 import type { APIRoute } from "astro";
 import { getCollection } from "astro:content";
 
-export const GET: APIRoute = async ({params,request})=>{
+export const prerender = false
+
+export const GET: APIRoute = async (request)=>{
  
   const posts = await  getCollection('blog')
 
+  console.log(request)
  
   return new Response(JSON.stringify(posts),{status:200,
     headers:{
