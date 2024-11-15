5b3ddb375d95384505326ad3737a9864aebc58ba
Nahuel-NP <pedroso.nahuel.dev@gmail.com>
2024-11-15 12:32:25 -0300

    implement POST, PUT, DELETE verb

--- src/pages/api/posts/[slug].ts
+++ src/pages/api/posts/[slug].ts
@@ -20,3 +20,36 @@ export const GET: APIRoute = async ({params})=>{
     'Content-Type':'application/json'
   }})
 }
+
+export const POST: APIRoute = async ({request})=>{
+
+  const body = await request.json()
+
+  return new Response(JSON.stringify(body),{status:200,headers:{
+    'Content-Type':'application/json'
+  }})
+}
+
+
+export const PUT: APIRoute = async ({request})=>{
+
+  const body = await request.json()
+
+  return new Response(JSON.stringify({
+    ...body,
+    method:'PUT'
+  }),{status:200,headers:{
+    'Content-Type':'application/json'
+  }})
+}
+export const DELETE: APIRoute = async ({request})=>{
+
+  const body = await request.json()
+
+  return new Response(JSON.stringify({
+    ...body,
+    method:'DELETE'
+  }),{status:200,headers:{
+    'Content-Type':'application/json'
+  }})
+}
