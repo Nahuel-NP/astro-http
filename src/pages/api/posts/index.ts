8d74682a894b2a3f91478fb8c525fa23493f3369
Nahuel-NP <pedroso.nahuel.dev@gmail.com>
2024-11-15 11:56:07 -0300

    return post by slug

--- src/pages/api/posts/index.ts
+++ src/pages/api/posts/index.ts
@@ -1,17 +1,44 @@
 import type { APIRoute } from "astro";
-import { getCollection } from "astro:content";
+import { getCollection, getEntry } from "astro:content";
 
 export const prerender = false
 
-export const GET: APIRoute = async (request)=>{
- 
-  const posts = await  getCollection('blog')
+export const GET: APIRoute = async (request) => {
 
-  console.log(request)
- 
-  return new Response(JSON.stringify(posts),{status:200,
-    headers:{
-      'Content-Type':'application/json'
+  const posts = await getCollection('blog')
+
+  const url = new URL(request.url)
+
+  const slug = url.searchParams.get('slug')
+
+  if (slug) {
+
+    const post = await getEntry('blog', slug)
+    if (post) {
+      return new Response(JSON.stringify(post), {
+        status: 200,
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+    }
+    else {
+      return new Response(JSON.stringify({ error: 'Post not found' }), {
+        status: 404,
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+    }
+  }
+
+
+  return new Response(JSON.stringify(posts), {
+    status: 200,
+    headers: {
+      'Content-Type': 'application/json'
     }
   })
+
+
 }
