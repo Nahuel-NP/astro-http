fb95a531dd031ff1af708dfe0a606f4e74cd6098
Nahuel-NP <pedroso.nahuel.dev@gmail.com>
2024-11-19 15:18:06 -0300

    create clients table and insert into seed

--- db/seed.ts
+++ db/seed.ts
@@ -1,6 +1,34 @@
-import { db } from 'astro:db';
+import { Clients, db } from 'astro:db';
 
 // https://astro.build/db/seed
 export default async function seed() {
 	// TODO
+
+	await db.insert(Clients).values([
+		{
+			id: 1,
+			name: 'John Doe',
+			age: 30,
+			isActive: true
+		},
+		{
+			id: 2,
+			name: 'Jane Doe',
+			age: 25,
+			isActive: false
+		},
+		{
+			id: 3,
+			name: 'John Smith',
+			age: 35,
+			isActive: true
+		},
+		{
+			id: 4,
+			name: 'Jane Smith',
+			age: 28,
+			isActive: true
+		}
+	])
+	console.log('seed executed')
 }
