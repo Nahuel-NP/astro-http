dd554c4b81054e5896aa7d3c768fb1efea7d3aa4
Nahuel-NP <pedroso.nahuel.dev@gmail.com>
2024-11-19 15:41:27 -0300

    define get and post methods

--- src/pages/api/clients/index.ts
+++ src/pages/api/clients/index.ts
@@ -1,25 +1,46 @@
 
 import type { APIRoute } from "astro";
+import { Clients, db } from "astro:db";
 export const prerender = false;
 export const GET: APIRoute = async (request) => {
 
-  return new Response(JSON.stringify({
-    method: 'GET'
-  }), {
+  const users = await db.select().from(Clients);
+
+  return new Response(JSON.stringify(
+    users
+  ), {
     status: 200,
     headers: {
       'Content-Type': 'application/json'
     }
   })
 }
-export const POST: APIRoute = async (request) => {
+export const POST: APIRoute = async ({ request }) => {
 
-  return new Response(JSON.stringify({
-    method: 'POST'
-  }), {
-    status: 200,
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  })
+  try {
+
+    const { id, ...body } = await request.json();
+
+    const { lastInsertRowid } = await db.insert(Clients).values(body);
+
+    return new Response(JSON.stringify({
+      ...body,
+      id: +lastInsertRowid?.toString()!
+    }), {
+      status: 200,
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+  } catch (error) {
+    console.log(error)
+    return new Response(JSON.stringify({
+      msg: 'No Body Found'
+    }), {
+      status: 200,
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+  }
 }
