abc71c0d8b699dec024b0e924be51e5ffb1e9e2e
Nahuel-NP <pedroso.nahuel.dev@gmail.com>
2024-11-21 18:37:01 -0300

    fetch like counts

--- src/components/likes/LikeCounter.vue
+++ src/components/likes/LikeCounter.vue
@@ -1,15 +1,53 @@
 <template>
-  <button @click="likePost">
-    Like Counter
+
+  <div v-if="isLoading">
+    Loading...
+  </div>
+  <button v-else-if="likeCount === 0" @click="likePost">
+    Like this post
+  </button>
+  <button v-else>
+    Likes <span>{{ likeCount }}</span>
   </button>
 </template>
 
 
 <script setup lang="ts">
+import { ref } from 'vue';
+
+
+interface Props {
+  postId: string;
+}
+
+const props = defineProps<Props>()
+
+
+const likeCount = ref(0)
+const likeClicks = ref(0)
+const isLoading = ref(true)
 
 const likePost = () => {
   console.log('+1 Like')
 }
+
+const getCurrentLikes = async ()=>{
+
+  const resp = await fetch(`/api/posts/likes/${props.postId}`)
+  if (!resp.ok) {
+    throw new Error('Failed to fetch likes')
+  }
+
+  const data = await resp.json();
+  console.log("🚀 ~ getCurrentLikes ~ data:", data)
+  
+  likeCount.value = data.likes;
+  isLoading.value = false
+}
+
+
+getCurrentLikes();
+
 </script>
 
 <style scoped>
